3687900e32a20a7b7f7a580a6109ddd74332c882
henriquedecambe <henrique.guimaraesgomes.martins@escola.pr.gov.br>
2025-10-02 09:02:13 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -55,3 +55,38 @@ titulo.addEventListener('click' , mostraMensagem);
 function mostraMensagem () {
     alert("este elemento foi clicado");
 }
+
+//acessa o botao
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+//executa os codigos ao clicar no botao
+botaoAdicionar.addEventListener('click',function(event){
+ event.preventDefault();
+
+ //acessa o formulario
+var formulario = document.querySelector("#form-adiciona");
+
+//captura os dados inseridos
+var nome = formulario.nome.value;
+var peso = formulario.peso.value;
+var altura = formulario.altura.value;
+var gordura = formulario.gordura.value;
+
+//criar a tag, <tr>
+var pacienteTr = document.createElement("tr");
+
+//cria as tags <td>
+var nomeTd = document.createElement("td");
+var pesoTd = document.createElement("td");
+var alturaTd = document.createElement("td");
+var gorduraTd = document.createElement("td");
+var imcTd = document.createElement("td");
+
+nomeTd.textContent = nome;
+pesoTd.textContent = peso;
+alturaTd.textContent = altura;
+gorduraTd.textContent = gordura;
+imcTd.textContent = imc;
+
+});
+
